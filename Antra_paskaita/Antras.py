0efecd4ad95025d5867780340dbbe530da083f36
Narimantas <narimantas97@gmail.com>
2023-01-05 19:29:06 +0200

    new commit

---
+++ Antra_paskaita/Antras.py
@@ -0,0 +1,49 @@
+# print('-----------')
+# print('mūsų darbas')
+# print(1987)
+# print('-----------')
+
+# print('mano tekstas')
+# print()
+# print('mano naujas tekstas ' * 3
+
+# tekstas = 'pirma eilute\nantra eilute\ntrecia eilute'
+# print(tekstas)
+# print('pirma eilute\nantra eilute\ntrecia eilute')
+
+# Vardas = 'Jonas'
+# Pavardė = 'Jonaitis'
+# Amžius = 25
+# kursas = 3
+# vidurkis = 8.7
+
+# formatuotas =f'{Vardas}{Pavardė}({Amžius}m.) mokosi {kursas}-ime kurse'
+#  formatuotas +=f'jo vidurkis yra {vidurkis}'
+
+# print(formatuotas)
+
+# vardas = 'Tomas'
+# pavarde = 'Tomauskas'
+# vidurkis = 8.7
+
+# # format yra senesnio python variantas teksto formatavimui
+# formatuotas = "Studento {} {} vidurkis yra {}".format(vardas, pavarde, vidurkis)
+
+# print(formatuotas)
+
+# print('Iveskite savo varda:')
+# vardas = input() # informacijos ivedimas is 
+# print('iveskite amziu')
+# amzius = input()
+# print =('iveskyte lyti')
+# lytis =input()
+# print('Jus ivedete:', vardas)
+
+print('Pirmas skaicius:')
+# pirmas = int(input())
+pirmas = float(input())
+print('Antras skaicius:')
+antras = int(input())
+print('Atsakymai:')
+print(f"{pirmas} + {antras} = {pirmas + antras}")
+print(f"{pirmas} - {antras} = {pirmas - antras}")
