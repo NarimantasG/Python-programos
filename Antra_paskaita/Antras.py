cef4762989b6ae35538ee4719e4716b34eacf406
Narimantas <narimantas97@gmail.com>
2023-01-05 20:33:38 +0200

    Pyton

--- Antra_paskaita/Antras.py
+++ Antra_paskaita/Antras.py
@@ -39,11 +39,106 @@
 # lytis =input()
 # print('Jus ivedete:', vardas)
 
-print('Pirmas skaicius:')
+# kintamasis int
+
+# print('Pirmas skaicius:')
+# # pirmas = int(input())
+# pirmas = float(input())
+# print('Antras skaicius:')
+# antras = int(input())
+# print('Atsakymai:')
+# print(f"{pirmas} + {antras} = {pirmas + antras}")
+# print(f"{pirmas} - {antras} = {pirmas - antras}")
+
+# print('Iveskite du skaicius (atskiriant enter paspaudimu):')
 # pirmas = int(input())
-pirmas = float(input())
-print('Antras skaicius:')
-antras = int(input())
-print('Atsakymai:')
-print(f"{pirmas} + {antras} = {pirmas + antras}")
-print(f"{pirmas} - {antras} = {pirmas - antras}")
+# antras = int(input())
+# suma = pirmas + antras
+# print('skaiciu suma:', suma)
+
+#if  else salygos
+
+# if 6 > 3:
+#     print('6 yra didesnis nei 3')
+# else if
+# elif
+#     print('6 lygu 4')
+# else:
+#     print('6 mažiau už 4')
+
+# if -7 > -10:
+#     print('-7 yra didesnis uz -10')
+# if 3 < 5:
+#     print('3 mazesnis uz 5')
+# if 8 < 0:
+#     print('8 mazesnis uz 0')
+
+# skaicius = 5
+# if skaicius > 0:
+#     print('skaicius teigiamas')
+# if skaicius < 0:
+#     print('skaicius neigiamas')
+# if skaicius == 0:
+#     print('skaicius lygus 0')
+
+# if 5 > 5:
+#   print('5 didesnis nei 5')
+# if 5 == 5:
+#   print('5 lygu 5')
+
+# slaptazodis = 'slaptas'
+# if slaptazodis == 'Slaptas':
+#     print('pirmas if')
+# if slaptazodis == 'slaptas':
+#     print('antras if')
+
+# print('Iveskite norima skaiciu:')
+# skaicius = int(input())
+# if skaicius > 0:
+#     print('ivestas skaicius teigiamas')
+# if skaicius < 0:
+#     print('ivestas skaicius neigiamas')
+# if skaicius == 0:
+#     print('ivestas skaicius lygus 0')
+
+# savas
+
+# print('įveskite skaičių nuo 10 iki -10')
+# skaicius = int(input())
+# if skaicius < 5:
+#     print(f'{skaicius} mazesnis uz 5')
+# if skaicius < 0:
+#     print(f'{skaicius} mazesnis uz 0')
+# if skaicius > -10:
+#     print(f'{skaicius} yra didesnis uz -10')
+
+#
+
+# skaicius = 8
+# if 5 + 3 >= 6:
+#     print('suveike 5 + 3 >= 6 patikrinimas')
+# if skaicius + 2 > 10:
+#     print('suveike', skaicius, '+ 2 > 10 patikrinimas')
+# if skaicius * 2 > 0:
+#     print('suveike', skaicius, '* 2 > 0 patikrinimas')
+# if 8 * 2 < 8 + 6:
+#     print('suveike 8 * 2 < 8 + 6 patikrinimas')
+
+# skaicius = int( input('Iveskite skaiciu: ') )
+# print(f'{skaicius}:')
+# if skaicius % 2 == 0:
+#     print('dalinasi is 2')
+# if skaicius % 3 == 0:
+#     print('dalinasi is 3')
+# if skaicius % 4 == 0:
+#     print('dalinasi is 4')
+# if skaicius % 5 == 0:
+#     print('dalinasi is 5')
+# if skaicius % 6 == 0:
+#     print('dalinasi is 6')
+# if skaicius % 7 == 0:
+#     print('dalinasi is 7')
+# if skaicius % 8 == 0:
+#     print('dalinasi is 8')
+# if skaicius % 9 == 0:
+#     print('dalinasi is 9')
---
+++ Pirma_paskaita/Užduotys/Užduotis_3.py
@@ -0,0 +1,90 @@
+#Užduotis 1-1
+
+# Vardas = 'Narimantas'
+# Amzius = 25
+# kodelBandauProgr = 'Noriu prasimokslinti sekančia '
+# kodelBandauProgr += 'programavimo kalba'
+# print (Vardas, Amzius)
+# print (kodelBandauProgr)
+
+#Užduotis 1-2
+
+# Tekstas ='Autorinės'
+# Vardas = 'Vytautas'
+# Pavarde ='Mačernis'
+# Metai = 1936
+# print ('Ramu į tėviškę sugrįžti')
+# print ('Keliais, kur veda į namus,')
+# print ('Ir čia kaip žydinčią jaunystę')
+# print ('Paklaust, kur išblaškys pasaulis mus.')
+# print (' ')
+# print ('Aš nebeklausiu liauno lino,')
+# print ('Kas laimę bėgančią pavys?')
+# print ('Užteks man vasarą palydint')
+# print ('Atmint saulėlydžių ugnis.')
+# print (' ')
+# print ('Nebedainuos dainų, lopšinių')
+# print ('Toli ten mieganti giria,')
+# print ('Tik vėjai išskalaus krūtinę')
+# print ('Ir neš, išneš mane iš čia.')
+# print (' ')
+# print ('Nors aš nenoriu melsvo šilko')
+# print ('Ir sesės saulės ant laukų,')
+# print ('Bet aš Tavęs, o Tėviške, išsilgau')
+# print ('Ir Tau vienai tikėt galiu.')
+# print (' ')
+# print (' ')
+# formatuotas =' teisės: {} {} Metai: {}'.format (Vardas, Pavarde ,Metai)
+# print (Tekstas + formatuotas)
+
+#Užduotis 1-3
+
+# print ('***')
+# print ('* *')
+# print ('***')
+
+#Užduotis 2-4
+
+# Pavadinimas = 'Leopardas'
+# Amzius = 20
+# Kailis ='Šviesiai geltonas '
+# Kailis +='su juodais taškeliais'
+# Svoris = 31
+# formatuotas =f"Gyvūnas - {Pavadinimas} ({Amzius} m.). Kailis yra "
+# formatuotas +=f"{Kailis} ir sveria {Svoris} kg. "
+# print (formatuotas)
+
+#Užduotis 2-5
+
+# sk = '25'
+# print (sk+sk+sk+sk+sk)
+
+#Užduotis 2-6
+
+# sk = 25
+# print (sk, sk, sk, sk, sk)
+
+#Užduotis 2-7
+
+# tekstas = '*\n**\n***'
+# print (tekstas)
+
+#Užduotis 2-8
+
+# tekstas ='Vienas\nDu\nTrys'
+# print (tekstas)
+
+#Užduotis 3-9
+
+# uzpildas = '+---------+---------+'
+# Vardas = 'Tomas'
+# Vardas2 = 'Jonas'
+# Vardas3 = 'Justė'
+# Amzius = 24
+# Amzius2 = 26
+# Amzius3 = 25
+# uzpildas1 = '| {}   | {}      |'.format(Vardas, Amzius)
+# uzpildas2 = '| {}   | {}      |'.format(Vardas2, Amzius2)
+# uzpildas3 = '| {}   | {}      |'.format(Vardas3, Amzius3)
+# turinys = f'{uzpildas}\n| Vardas  | Amžius  |\n{uzpildas}\n{uzpildas1}\n{uzpildas2}\n{uzpildas3}\n{uzpildas}'
+# print (turinys)
