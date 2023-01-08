685744110fbdeaefcc31e4344f5747664391b899
Narimantas <narimantas97@gmail.com>
2023-01-08 16:21:29 +0200

    Paskaita 2

---
+++ Antra_paskaita/Užduotys/Užduotis_4.py
@@ -0,0 +1,33 @@
+#Užduotis 1-1
+
+# suma = 8 * 4 + 2
+# suma2 = 8 * (4 + 2)
+# suma3 = 48 / 4
+# suma4 = 3 + 6 * 2
+# print(f'atsakymai:\n{suma}\n{suma2}\n{suma3}\n{suma4}')
+
+#Užduotis 1-2
+
+# sk1 = 5
+# sk2 = 9
+# bendsuma = sk1 + sk2
+# print(f'Skaičius 1: {sk1}\nSkaičius 2: {sk2}\nSuma: {bendsuma}')
+
+#Užduotis 2-3
+
+# kint1 = 5
+# kint2 = 9
+# kint3 = 2
+# suma1=kint1+kint2+kint3
+# suma2=kint1-kint2-kint3
+# suma3=kint1*kint2*kint3
+# suma4=kint1/kint2/kint3
+# print(f'plius {suma1}, Minus {suma2}, Daugint {suma3}, Dalint {suma4}')
+
+#Užduotis 2-4
+
+sk1 = 10
+sk2 = 15
+rezult = sk1+5
+rezult2 =sk2*2
+print(f'Pradinis: {sk1}, Rezultatas: {rezult}\nPradinis: {sk2}, Rezultatas: {rezult2}')
---
+++ Antra_paskaita/Užduotys/Užduotis_5.py
@@ -0,0 +1,88 @@
+#Užduotis 1-1
+
+# print ('Įveskite Varda')
+# vardas=input()
+# print ('Įveskite Pavardę')
+# pavard=input()
+# print ('Kodėl pasirinkot programavima')
+# preizastis=input()
+# print(f'Aš esu{vardas} {pavard} {preizastis}')
+
+#Užduotis 1-2
+
+# print('Įveskite simboli')
+# simbolis=input()
+# print(f'Įvestas Simbolis {simbolis}\n{simbolis}{simbolis}{simbolis}\n{simbolis}{simbolis}{simbolis}\n{simbolis}{simbolis}{simbolis}')
+
+#Užduotis 1-3
+
+# print('Įveskite simboli')
+# simbolis=input()
+# print(f'Įvestas Simbolis {simbolis}\n{simbolis}\n{simbolis}{simbolis}\n{simbolis}{simbolis}{simbolis}')
+
+#Užduotis 2-4
+
+# print('Vardas')
+# Vardas=input()
+# print('Amžius')
+# Amzius=int(input())
+# print('Svoris')
+# Svoris=float(input())
+# print(f'Vardas: {Vardas}', type(Vardas),f'\nAmžius: {Amzius}', type(Amzius), f'\nSvoris: {Svoris}', type(Svoris))
+
+#Užduotis 2-5
+
+# print('Įveskite pažymius')
+# print('Pirmas')
+# pirmas=int(input()) 
+# print('Antras')
+# antras=int(input()) 
+# print('Trečias')
+# trecias=int(input()) 
+# print('Ketvirtas')
+# ketvirtas=int(input()) 
+# print('penktas')
+# penktas=int(input()) 
+# suma=pirmas+antras+trecias+ketvirtas+penktas
+# vidurkis=suma/5
+# print('Vidurkis', vidurkis)
+
+#Užduotis 2-6
+
+# print('Įveskite metus')
+# metai=float(input())
+# mm=metai*10
+# km=metai*0.00001
+# print(f'Centimetrais: {metai}')
+# print(f'Milimetrais: {mm}')
+# print(f'Kilometrais: {km}')
+
+#Užduotis 2-7
+
+# print('Skaicius 1')
+# sk1=float(input())
+# print('Skaicius 2')
+# sk2=float(input())
+# suma=sk1+sk2
+# skirtumas=sk1-sk2
+# sandauga=sk1*sk2
+# dalyba=sk1/sk2
+# print(f'Suma: {suma}, Skirtumas: {skirtumas}, Sandauga: {sandauga}, Dalinta: {dalyba}')
+
+#Užduotis 3-8
+
+# print('skaičius 1')
+# sk1=int(input())
+# print('skaičius 2')
+# sk2=int(input())
+# suma=sk1%sk2
+# print('Liekana :', suma)
+
+#Užduotis 3-9
+
+print('skaičius 1')
+sk1=int(input())
+print('skaičius 2')
+sk2=int(input())
+suma=sk1//sk2
+print(f'Skaičius {sk1}, pakeltas {sk2} laipsnių rezultatas :', suma)
