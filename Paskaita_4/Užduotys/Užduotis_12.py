9aa2cc9453deba10a744e2d744e338c6c1221026
NarimantasG <90923845+NarimantasG@users.noreply.github.com>
2023-01-17 02:58:53 +0200

    python

---
+++ Paskaita_4/Užduotys/Užduotis_12.py
@@ -0,0 +1,21 @@
+Studentas={
+'Studentas':{
+'Vardas':'Vardenis',
+'Pavardė':'Pavardenis',
+'Amžius':25,
+'ūgis':1.95},
+'Kur mokosi':{ 
+'Mokykla':'Vilnius Coding School',
+'Kursas':20,
+'Studijų programa':15,},
+'Kreditų skaičius':{'atsiskaitytų kreditų skaičius':20},
+'Pažymiai':{5,10,9,7,8,5}
+}
+
+
+print('Studentas: ',Studentas)
+print('Žmogus: ',Studentas['Studentas'])
+print('Kur mokosi: ',Studentas['Kur mokosi'])
+print('Kreditų skaičius: ',Studentas['Kreditų skaičius'])
+print('Pažymiai: ',Studentas['Pažymiai'])
+
